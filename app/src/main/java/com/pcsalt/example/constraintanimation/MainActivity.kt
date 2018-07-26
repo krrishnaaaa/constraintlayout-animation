1d53af1621bc82083d06d4c4cb974576e132294a
krrishnaaaa <krishna@pcsalt.com>
2018-07-27 01:42:03 +0530

    Initial commit

---
+++ app/src/main/java/com/pcsalt/example/constraintanimation/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.pcsalt.example.constraintanimation
+
+import android.os.Bundle
+import android.support.constraint.ConstraintSet
+import android.support.v7.app.AppCompatActivity
+import android.transition.ChangeBounds
+import android.transition.Scene
+import android.transition.Transition
+import android.transition.TransitionManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        initBackground()
+
+        initConstraint()
+
+    }
+
+    private fun initBackground() {
+    }
+
+    private fun initConstraint() {
+        val sqConstraint = ConstraintSet()
+        sqConstraint.clone(cl)
+
+        val cirConstraint = ConstraintSet()
+        cirConstraint.clone(this, R.layout.activity_main_alt)
+
+        var set = false
+        btn.setOnClickListener {
+            val changeBounds = ChangeBounds()
+            changeBounds.addListener(object : Transition.TransitionListener {
+                override fun onTransitionStart(transition: Transition) {
+                    iv.setIsCircle(set)
+                    iv.invalidate()
+                }
+
+                override fun onTransitionEnd(transition: Transition) {
+                }
+
+                override fun onTransitionCancel(transition: Transition) {}
+
+                override fun onTransitionPause(transition: Transition) {}
+
+                override fun onTransitionResume(transition: Transition) {}
+            })
+
+            TransitionManager.go(Scene(cl), changeBounds)
+            val constraint = if (set) sqConstraint else cirConstraint
+            constraint.applyTo(cl)
+            set = !set
+        }
+    }
+}
